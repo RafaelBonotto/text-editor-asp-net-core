f01c3b6aa0d85708c18e8aaaab26db392ef9be4d
RafaelBonotto <68786414+RafaelBonotto@users.noreply.github.com>
2022-04-14 12:04:50 -0300

    Update EditorDeArquivos.cs

--- Editor/EditorDeArquivos.cs
+++ Editor/EditorDeArquivos.cs
@@ -1,10 +1,26 @@
-﻿namespace Editor
+﻿using System;
+using System.IO;
+
+namespace Editor
 {
     public class EditorDeArquivos
     {
         public void Abrir()
         {
+            Console.Clear();
+            Console.WriteLine("Qual caminho do arquivo?");
+            string path = Console.ReadLine();
 
+            using (var file = new StreamReader(path))
+            {
+                string text = file.ReadToEnd();
+                Console.WriteLine(text);
+            }
+
+            Console.WriteLine("");
+            Console.ReadLine();
+            Menu menu = new();
+            menu.ExibirMenu();
         }
 
         public void Editar()
