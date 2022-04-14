8c5e92c93e2c6552a422d08740eb68427df9b5b4
RafaelBonotto <68786414+RafaelBonotto@users.noreply.github.com>
2022-04-14 12:03:32 -0300

    Update Menu.cs

--- Editor/Menu.cs
+++ Editor/Menu.cs
@@ -1,7 +1,26 @@
-﻿namespace Editor
+﻿using System;
+
+namespace Editor
 {
     public class Menu
     {
+        public void ExibirMenu()
+        {
+            Console.Clear();
+            Console.WriteLine("O que você deseja fazer?");
+            Console.WriteLine("1 - Abrir arquivo");
+            Console.WriteLine("2 - Criar novo arquivo");
+            Console.WriteLine("0 - Sair");
+            short option = short.Parse(Console.ReadLine());
+            EditorDeArquivos editorDeArquivos = new();
 
+            switch (option)
+            {
+                case 0: System.Environment.Exit(0); break;
+                case 1: editorDeArquivos.Abrir(); break;
+                case 2: editorDeArquivos.Editar(); break;
+                default: this.ExibirMenu(); break;
+            }
+        }
     }
 }
